a430f6b7385350f2a3c5638cafb3b40d33ad27b9
wangxiaoyu <wangxiaoyu.tracy@bytedance.com>
2022-03-21 22:43:33 +0800

    optimize macd policy

--- policy/macd.go
+++ policy/macd.go
@@ -7,48 +7,83 @@ import (
 )
 
 type MacdPolicy struct{}
+type condition struct {
+	coin        string
+	dataMacd15M []map[string]string
+	dataMacd4H  []map[string]string
+}
 
 // find macd buy point
 func (*MacdPolicy) Target(args ...interface{}) interface{} {
 	// convert specified type
 	coin := args[0].(string)
 
-	k4hValues := (&interfaces.MarketArgs{
+	market4H := (&interfaces.MarketArgs{
 		CurrencyPair: coin,
 		Interval:     -100,
 		Level:        utils.Level4Hour,
 	}).Market()
-	k15mValues := (&interfaces.MarketArgs{
+	market15M := (&interfaces.MarketArgs{
 		CurrencyPair: coin,
 		Interval:     -1,
 		Level:        utils.Level15Min,
 	}).Market()
 
-	if k4hValues != nil && k15mValues != nil {
+	if market4H != nil && market15M != nil {
 		macdArgs := index.DefaultMacdArgs()
-		k4hMacds := macdArgs.GetMacd(k4hValues)
-		k15mMacds := macdArgs.GetMacd(k15mValues)
-		// fetch now local time macd data
-		nowK4h := len(k4hMacds) - 1
-		nowK15m := len(k15mMacds) - 1
+		c := &condition{
+			coin:        coin,
+			dataMacd15M: macdArgs.GetMacd(market15M),
+			dataMacd4H:  macdArgs.GetMacd(market4H),
+		}
 
-		if nowK4h < 5 || nowK15m < 5 {
+		if len(c.dataMacd4H) < 5 || len(c.dataMacd15M) < 5 {
 			return false
 		}
 
-		// set conditions
-		H4Up := utils.StringToFloat32(k4hMacds[nowK4h]["c"]) > utils.StringToFloat32(k4hMacds[nowK4h]["o"])
-		H4Up1 := utils.StringToFloat32(k4hMacds[nowK4h-1]["c"]) > utils.StringToFloat32(k4hMacds[nowK4h-2]["c"])*1.1
-		H4Up2 := utils.StringToFloat32(k4hMacds[nowK4h-2]["c"]) > utils.StringToFloat32(k4hMacds[nowK4h-3]["c"])*1.1
-
-		macdValue := utils.StringToFloat32(k4hMacds[nowK4h]["macd"])
-		priceDiff := (utils.StringToFloat32(k4hMacds[nowK4h]["c"]) - utils.StringToFloat32(k4hMacds[nowK4h]["o"])) / utils.StringToFloat32(k4hMacds[nowK4h]["o"])
-		// Increasing trading 15min macd
-		k15MacDiff := utils.StringToFloat32(k15mMacds[nowK15m]["macd"]) >= utils.StringToFloat32(k15mMacds[nowK15m-1]["macd"])
-		k15Macd := utils.StringToFloat32(k15mMacds[nowK15m]["macd"])
-
-		return macdValue > 0 && H4Up && H4Up1 && H4Up2 && priceDiff >= 0.05 && k15MacDiff && k15Macd > 0
+		return c.buyCondition()
 	}
 
 	return false
+}
+
+func (c *condition) buyCondition() bool {
+	// judgment depends on 4h price
+	conditionA := compare(c.dataMacd4H[len(c.dataMacd4H)-1][utils.Close], c.dataMacd4H[len(c.dataMacd4H)-1][utils.Open], 0, 0)    //当下4h是具有涨幅的
+	conditionB := compare(c.dataMacd4H[len(c.dataMacd4H)-2][utils.Close], c.dataMacd4H[len(c.dataMacd4H)-3][utils.Close], 0, 1.1) //上个4h是涨幅十个点以上的
+
+	// judgment depends on 4h macd
+	conditionC := utils.StringToFloat32(c.dataMacd4H[len(c.dataMacd4H)-1]["macd"]) > 0                                    //当下4h的macd大于0
+	conditionD := utils.StringToFloat32(c.dataMacd15M[len(c.dataMacd15M)-1]["macd"]) > 0                                  //当下15m的macd大于0
+	conditionE := compare(c.dataMacd15M[len(c.dataMacd15M)-1]["macd"], c.dataMacd15M[len(c.dataMacd15M)-2]["macd"], 0, 0) //15m的macd是整长的
+
+	// judgment depends on price
+	conditionF := compare(c.dataMacd4H[len(c.dataMacd4H)-1][utils.Close], c.dataMacd4H[len(c.dataMacd4H)-1][utils.Open], 0, 1.1)
+
+	// judgment depends on price average data
+	averageArgs := index.Average{
+		CurrencyPair: c.coin,
+		Intervel:     utils.Five,
+		Level:        utils.Level15Min,
+	}
+	conditionG := averageArgs.FiveAverage(false) > averageArgs.FiveAverage(false)
+
+	return conditionA && conditionB && conditionC && conditionD && conditionE && conditionF && conditionG
+}
+
+// if compareA is larger than compareB, then return true
+func compare(compareA string, compareB string, weightA float32, weightB float32) bool {
+	// string to float32
+	compareAf := utils.StringToFloat32(compareA)
+	compareBf := utils.StringToFloat32(compareB)
+
+	if weightA != 0 {
+		compareAf = compareAf * weightA
+	}
+
+	if weightB != 0 {
+		compareBf = compareBf * weightB
+	}
+
+	return compareAf > weightB
 }
--- utils/const.go
+++ utils/const.go
@@ -1,12 +1,14 @@
 package utils
 
 const (
-	Five                    = -4 // if you want to get 5 days market data, here should be typed -4
-	Six                     = -5
-	Seven                   = -6
-	Ten                     = -9
-	Level15Min              = "15m"
-	Level4Hour              = "4h"
-	Level1Day               = "1d"
-	DBHistoryDayUniq string = "UNIQUE constraint failed: history_day.contract, history_day.time"
+	Five             = -4 // if you want to get 5 days market data, here should be typed -4
+	Six              = -5
+	Seven            = -6
+	Ten              = -9
+	Level15Min       = "15m"
+	Level4Hour       = "4h"
+	Level1Day        = "1d"
+	Close            = "c"
+	Open             = "o"
+	DBHistoryDayUniq = "UNIQUE constraint failed: history_day.contract, history_day.time"
 )
