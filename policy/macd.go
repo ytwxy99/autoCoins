42d9ca9ec752a34c577ded02ec87ef35253730aa
wangxiaoyu <wangxiaoyu.tracy@bytedance.com>
2022-03-29 14:50:08 +0800

    trend policy

--- policy/macd.go
+++ policy/macd.go
@@ -57,20 +57,16 @@ func (c *condition) buyCondition() bool {
 	conditionD := utils.StringToFloat32(c.dataMacd15M[len(c.dataMacd15M)-1]["macd"]) > 0                                        //当下15m的macd大于0
 	conditionE := utils.Compare(c.dataMacd15M[len(c.dataMacd15M)-1]["macd"], c.dataMacd15M[len(c.dataMacd15M)-2]["macd"], 0, 0) //15m的macd是增长的
 
-	// judgment depends on price
-	conditionF := utils.Compare(c.dataMacd4H[len(c.dataMacd4H)-1][utils.Close], c.dataMacd4H[len(c.dataMacd4H)-1][utils.Open], 0, 1.1) //当前价格涨幅超过10%
-	conditionG := utils.Compare(c.dataMacd4H[len(c.dataMacd4H)-2][utils.Close], c.dataMacd4H[len(c.dataMacd4H)-2][utils.Open], 0, 1.1) //上一个4h价格涨幅超过10%
-
 	// judgment depends on price average data
 	averageArgs := index.Average{
 		CurrencyPair: c.coin,
 		Intervel:     utils.Five,
 		Level:        utils.Level4Hour,
 	}
-	conditionH := averageArgs.FiveAverage(false) > averageArgs.FiveAverage(true) //4h的FiveAverage是增长的
+	conditionF := averageArgs.FiveAverage(false) > averageArgs.FiveAverage(true) //4h的FiveAverage是增长的
 
 	//// judgment depends on volume
 	//conditionI := compare(c.dataMacd4H[len(c.dataMacd4H)-2][utils.Volume], c.dataMacd4H[len(c.dataMacd4H)-3][utils.Volume], 0, 1.1) //volme 增长10%
 
-	return conditionA && conditionB && conditionC && conditionD && conditionE && conditionF && conditionG && conditionH
+	return conditionA && conditionB && conditionC && conditionD && conditionE && conditionF
 }
